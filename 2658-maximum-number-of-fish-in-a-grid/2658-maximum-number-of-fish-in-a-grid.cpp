7a8accc675ef2c3f0ec8de695a07e05d16a8fcbb
Dnyaneshwar Bajad <156324900+DnyaneshwarBajad@users.noreply.github.com>
2026-07-24 21:40:32 +0530

    Time: 2 ms (85.26%), Space: 97.1 MB (42.1%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -23,12 +23,36 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
 
+        queue<pair<int, int>> q;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                int fish = 0;
                 if (grid[i][j] != 0) {
+                    q.push({i, j});
 
-                    ans = max(ans, dfs(grid, i, j));
+                    while (!q.empty()) {
+                        auto [r, c] = q.front();
+                        q.pop();
+                        fish += grid[r][c];
+                        grid[r][c] = 0;
+
+                        for (int i = 0; i < 4; i++) {
+                            int nr = dr[i] + r;
+                            int nc = dc[i] + c;
+                            if (nr >= 0 && nr < m && nc >= 0 && nc < n &&
+                                grid[nr][nc] > 0) {
+                                
+                                fish += grid[nr][nc];
+                                grid[nr][nc] = 0;
+                                q.push({nr, nc});
+                            }
+                        }
+                    }
+
+                    ans = max(ans, fish);
                 }
             }
         }
